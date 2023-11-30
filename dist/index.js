1412311f087c1c64360d1f8d10d2b46779ae8858
Tomasz Suski <tomasz.suski@orba.co>
2023-11-30 13:20:19 +0100

    ts classes, their differences from js classes

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,53 @@
 "use strict";
-// wykrzyknik zaznacza element jako istniejący na pewno, nie null. Eliminuje union type z null.
-const btn = document.getElementById("btn");
-const input = document.getElementById("todoinput");
-// wyszukanie po typie elementu podpowiada TStyp
-const form = document.querySelector("form");
-// można określić typ elementu w momencie wywołania zamiast przypisywać przy definiowaniu
-// (<HTMLButtonElement>btn).addEventListener("click", function () {
-//   const inputValue: string = input.value;
-//   alert(inputValue);
-//   input.value = "";
-// });
-function handleSubmit(e) {
-    e.preventDefault();
-    console.log("Submitted");
+class Player {
+    // w TS wszystkie properties muszą być typowane. Również te tworzone w konstruktorze. Typy wpisuje się po prostu w klasę.
+    // public jest słowem kluczowym pozwalającym wyraźnie zaznaczyć, że konkretne property czy metoda są dostępne na zewnątrz.
+    // Wszystko jest domyślnie public. Stosuje się to ewentualnie dla czytelności kodu
+    firstName;
+    // readonly pozwala jedynie na odczyt po zainicjowaniu w konstruktorze
+    lastName;
+    // private wyłącza dostępność danego elementu spoza klasy. Oczywiście działa JSowe "#" (#score). Nie da się używac obu naraz.
+    // private nie dodaje hasha, tylko przed kompilacją TS wskazuje niedostępność!
+    // private jest jednostepne z zewnątrz, również dla klas dziedziczących
+    // w JS nazwa "#score" i "score" są różne, w TS żeby móc dodać geter i seter o nazwie score, trzeba użyc innej nazwy dla elementu prywatnego (np. "_score")
+    _score = 0;
+    // protected wyłącza dostęp spoza klasy, ale pozwala na dziedziczenie (klasy dziedziczące też wyłączają dostęp spoza nich)
+    setScore(newScore) {
+        this._score = newScore;
+    }
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    // geter umożliwia wpisanie typu zwracanej wartości, choć oczywiście TS wykrywa ją sam
+    get score() {
+        return this._score;
+    }
+    // seter nie może nic zwracać i w związku z tym TS nie pozwala na przypisanie zwracanego typu - nawet void!
+    set score(newScore) {
+        if (newScore < 0) {
+            throw new Error("Score cannot be negative");
+        }
+        this._score = newScore;
+    }
 }
-form.addEventListener("submit", handleSubmit);
+class PlayerTheDifferentWay {
+    firstName;
+    lastName;
+    // można użyć innego zapisu, bez typowania properties, które będą inicjowane w konstruktorze.
+    // W tym celu trzeba określić dostępność parametrów (public, private, protected).
+    // W tym przypadku nie trzeba inicjalizować tych elementów z użyciem "this"!
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+class InheritedPlayer extends Player {
+    set maxScore(newScore) {
+        this.setScore(newScore);
+    }
+}
+const player = new InheritedPlayer("John", "Doe");
+console.log(player);
+player.maxScore = 11111;
+console.log(player.score);
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,53 @@
-// wykrzyknik zaznacza element jako istniejący na pewno, nie null. Eliminuje union type z null.
-const btn = document.getElementById("btn")!;
-const input = document.getElementById("todoinput")! as HTMLInputElement;
-// wyszukanie po typie elementu podpowiada TStyp
-const form = document.querySelector("form")!;
+class Player {
+  // w TS wszystkie properties muszą być typowane. Również te tworzone w konstruktorze. Typy wpisuje się po prostu w klasę.
+  // public jest słowem kluczowym pozwalającym wyraźnie zaznaczyć, że konkretne property czy metoda są dostępne na zewnątrz.
+  // Wszystko jest domyślnie public. Stosuje się to ewentualnie dla czytelności kodu
+  public firstName: string;
+  // readonly pozwala jedynie na odczyt po zainicjowaniu w konstruktorze
+  public readonly lastName: string;
+  // private wyłącza dostępność danego elementu spoza klasy. Oczywiście działa JSowe "#" (#score). Nie da się używac obu naraz.
+  // private nie dodaje hasha, tylko przed kompilacją TS wskazuje niedostępność!
+  // private jest jednostepne z zewnątrz, również dla klas dziedziczących
+  // w JS nazwa "#score" i "score" są różne, w TS żeby móc dodać geter i seter o nazwie score, trzeba użyc innej nazwy dla elementu prywatnego (np. "_score")
+  private _score: number = 0;
+  // protected wyłącza dostęp spoza klasy, ale pozwala na dziedziczenie (klasy dziedziczące też wyłączają dostęp spoza nich)
+  protected setScore(newScore: number) {
+    this._score = newScore;
+  }
 
-// można określić typ elementu w momencie wywołania zamiast przypisywać przy definiowaniu
-// (<HTMLButtonElement>btn).addEventListener("click", function () {
-//   const inputValue: string = input.value;
-//   alert(inputValue);
-//   input.value = "";
-// });
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
-function handleSubmit(e: SubmitEvent): void {
-  e.preventDefault();
-  console.log("Submitted");
+  // geter umożliwia wpisanie typu zwracanej wartości, choć oczywiście TS wykrywa ją sam
+  get score(): number {
+    return this._score;
+  }
+  // seter nie może nic zwracać i w związku z tym TS nie pozwala na przypisanie zwracanego typu - nawet void!
+  set score(newScore: number) {
+    if (newScore < 0) {
+      throw new Error("Score cannot be negative");
+    }
+    this._score = newScore;
+  }
 }
 
-form.addEventListener("submit", handleSubmit);
+class PlayerTheDifferentWay {
+  // można użyć innego zapisu, bez typowania properties, które będą inicjowane w konstruktorze.
+  // W tym celu trzeba określić dostępność parametrów (public, private, protected).
+  // W tym przypadku nie trzeba inicjalizować tych elementów z użyciem "this"!
+  constructor(public firstName: string, public lastName: string) {}
+}
+
+class InheritedPlayer extends Player {
+  set maxScore(newScore: number) {
+    this.setScore(newScore);
+  }
+}
+
+const player = new InheritedPlayer("John", "Doe");
+console.log(player);
+player.maxScore = 11111;
+console.log(player.score);
+
