bfb5fb60ddd60f330c9c739496724761b1ce60f8
Tomasz Suski <tomasz.suski@orba.co>
2023-12-01 15:16:38 +0100

    Typeof guards

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,7 @@
 "use strict";
-// wykrzyknik zaznacza element jako istniejący na pewno, nie null. Eliminuje union type z null.
-const btn = document.getElementById("btn");
-const input = document.getElementById("todoinput");
-// wyszukanie po typie elementu podpowiada TStyp
-const form = document.querySelector("form");
-// można określić typ elementu w momencie wywołania zamiast przypisywać przy definiowaniu
-// (<HTMLButtonElement>btn).addEventListener("click", function () {
-//   const inputValue: string = input.value;
-//   alert(inputValue);
-//   input.value = "";
-// });
-function handleSubmit(e) {
-    e.preventDefault();
-    console.log("Submitted");
+// dla primitives można użyć "typeof guard"
+function triple(value) {
+    if (typeof value === "string")
+        return value.repeat(3);
+    return value * 3;
 }
-form.addEventListener("submit", handleSubmit);
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,5 @@
-// wykrzyknik zaznacza element jako istniejący na pewno, nie null. Eliminuje union type z null.
-const btn = document.getElementById("btn")!;
-const input = document.getElementById("todoinput")! as HTMLInputElement;
-// wyszukanie po typie elementu podpowiada TStyp
-const form = document.querySelector("form")!;
-
-// można określić typ elementu w momencie wywołania zamiast przypisywać przy definiowaniu
-// (<HTMLButtonElement>btn).addEventListener("click", function () {
-//   const inputValue: string = input.value;
-//   alert(inputValue);
-//   input.value = "";
-// });
-
-function handleSubmit(e: SubmitEvent): void {
-  e.preventDefault();
-  console.log("Submitted");
+// dla primitives można użyć "typeof guard"
+function triple(value: number | string) {
+  if (typeof value === "string") return value.repeat(3);
+  return value * 3;
 }
-
-form.addEventListener("submit", handleSubmit);
