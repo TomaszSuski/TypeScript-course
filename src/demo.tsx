45f8af42f7d7700253cc702a02d00ba92f5bbb82
Tomasz Suski <tomasz.suski@orba.co>
2023-12-01 11:58:34 +0100

    arrow functions & tsx - special syntax small fix

--- src/demo.tsx
+++ src/demo.tsx
@@ -1,12 +1,12 @@
 // w tsx nie ma problemu z taką definicją funkcji i nawiasem strzałkowym okreslającym typ
-function getRandomElement<T>(list: T[]): T {
+function getRandomElement2<T>(list: T[]): T {
   return list[Math.floor(Math.random() * list.length)];
 }
 
 // jednak w przypadku funkcji strzałkowych już tak
 // trzeba dodac przecinek po nazwie typu!
-const getRandomElement2 = <T,>(list: T[]): T => {
+const getRandomElement3 = <T,>(list: T[]): T => {
   return list[Math.floor(Math.random() * list.length)];
 };
 
-getRandomElement2(["a", "b"]);
+getRandomElement3(["a", "b"]);
