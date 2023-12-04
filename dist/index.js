1f705bc2be83e386121dd96e4db2de16ea78d920
Tomasz Suski <tomasz.suski@orba.co>
2023-12-04 12:55:29 +0100

    working with axios

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,24 @@
 "use strict";
+// type declaration files - specjalny typ plików z rozszerzeniem .d.ts
+// mogą zawierać wyłącznie deklaracje typów, bez żadnego wykonywalnego kodu
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// w tej części będą wykonywane requesty za pomocą biblioteki axios. npm install axios przed rozpoczęciem
+const axios_1 = __importDefault(require("axios"));
+const URL = "https://jsonplaceholder.typicode.com/users/1";
+axios_1.default
+    // get przyjmuje generic type który można okreslić i zwraca response. TS będzie oczekiwał, że response jest typu przekazanego
+    .get(URL)
+    .then((resp) => {
+    console.log(resp.data);
+    printUser(resp.data);
+})
+    .catch((e) => {
+    console.log("error", e);
+});
+// zdefiniowanie generic type dla get pozwala np. używać funkcji przetwarzającej dane, nie tylko bez zwracania przez TS uwagi na błędy, ale też z podpowiedziami ide przy okazji
+function printUser(user) {
+    console.log(user.name);
+}
---
+++ src/index.ts
@@ -0,0 +1,47 @@
+// type declaration files - specjalny typ plików z rozszerzeniem .d.ts
+// mogą zawierać wyłącznie deklaracje typów, bez żadnego wykonywalnego kodu
+
+// w tej części będą wykonywane requesty za pomocą biblioteki axios. npm install axios przed rozpoczęciem
+
+import axios from "axios";
+const URL = "https://jsonplaceholder.typicode.com/users/1";
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+axios
+  // get przyjmuje generic type który można okreslić i zwraca response. TS będzie oczekiwał, że response jest typu przekazanego
+  .get<User>(URL)
+  .then((resp) => {
+    console.log(resp.data);
+    printUser(resp.data);
+  })
+  .catch((e) => {
+    console.log("error", e);
+  });
+
+// zdefiniowanie generic type dla get pozwala np. używać funkcji przetwarzającej dane, nie tylko bez zwracania przez TS uwagi na błędy, ale też z podpowiedziami ide przy okazji
+function printUser(user: User): void {
+  console.log(user.name);
+}
