77cd8b0f8e09c86e568480f55ac6d68fb6bad0f2
Tomasz Suski <tomasz.suski@orba.co>
2023-12-04 13:35:53 +0100

    using TS modules

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,11 @@
 "use strict";
+// typescript zakłąda, że wszystko w plikach TS jest we wspólnym global scope i pozwala używać zmiennych czy funkcji z innych plików
+// w momencie kompilacji jednak, JS trakutje pliki bez export (albo to-plevel await) jako skrypty a nie moduły i w związku z tym ten dostęp jest utracony,
+// chyba że zostaną te ppliki załadowane jako skrypty w odpowiedniej kolejności np. w pliku html
+// w tym wypadku TS rozpoznaje i pozwala używać metody z pliku utils.ts, ale po kompilacji to przestanie działać
+Object.defineProperty(exports, "__esModule", { value: true });
+// dopiero w momencie eksportu czegokolwiek z pliku utils.ts, TS zacznie zwracać uwagę na błąd braku importu i niemoznośc użycia tego, co nie zostało wyeksportowane
+// trzeba przy tym pamiętać, że importuje się pliki .js bo to one po kompilacji będą używane:
+const utils_js_1 = require("./utils.js");
+console.log((0, utils_js_1.add)(1, 2));
+;
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// typescript zakłąda, że wszystko w plikach TS jest we wspólnym global scope i pozwala używać zmiennych czy funkcji z innych plików
+// w momencie kompilacji jednak, JS trakutje pliki bez export (albo to-plevel await) jako skrypty a nie moduły i w związku z tym ten dostęp jest utracony,
+// chyba że zostaną te ppliki załadowane jako skrypty w odpowiedniej kolejności np. w pliku html
+// w tym wypadku TS rozpoznaje i pozwala używać metody z pliku utils.ts, ale po kompilacji to przestanie działać
+
+// dopiero w momencie eksportu czegokolwiek z pliku utils.ts, TS zacznie zwracać uwagę na błąd braku importu i niemoznośc użycia tego, co nie zostało wyeksportowane
+// trzeba przy tym pamiętać, że importuje się pliki .js bo to one po kompilacji będą używane:
+import { add } from "./utils.js";
+
+console.log(add(1, 2););
