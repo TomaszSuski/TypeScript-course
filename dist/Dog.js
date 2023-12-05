0cd0c4cad9025e9605a9da97d84c4cb6ba050640
Tomasz Suski <tomasz.suski@orba.co>
2023-12-05 10:45:29 +0100

    added source maps

---
+++ dist/Dog.js
@@ -0,0 +1,14 @@
+export default class Dog {
+    name;
+    breed;
+    age;
+    constructor(name, breed, age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+    bark() {
+        console.log("Woof");
+    }
+}
+//# sourceMappingURL=Dog.js.map
