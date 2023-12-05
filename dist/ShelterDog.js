af53d5a0e4b1e7903c188e35fdb1126d215dc9eb
Tomasz Suski <tomasz.suski@orba.co>
2023-12-05 10:39:54 +0100

    webpack configuration

--- dist/ShelterDog.js
+++
@@ -1,8 +0,0 @@
-import Dog from "./Dog.js";
-export default class ShelterDog extends Dog {
-    shelter;
-    constructor(name, breed, age, shelter) {
-        super(name, breed, age);
-        this.shelter = shelter;
-    }
-}
